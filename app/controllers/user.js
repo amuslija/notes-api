610fbbb6eb3bac8fb7ccfdb33e32f0085be2e6c3
Adnan Muslija <muslija.adnan@gmail.com>
2017-07-04 13:18:54 +0200

    minor project restructure

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,10 +2,14 @@ var User = require('../models/user');
 
 module.exports = {
   create: function (req, res, next) {
+    console.log(req);
     var email = req.body.email;
     var password = req.body.password;
 
-    var user = new User({});
+    var user = new User({
+      email: email,
+      password: password
+    });
 
     user.save(function (err) {
       if (err) {
